10148c53547d92345c8b78adf0e9df5a4b5481f4
Janne Liuhtonen <janne.liuhtonen@iki.fi>
2023-09-16 08:41:40 +0300

    Remove duplicate code

--- src/util.ts
+++ src/util.ts
@@ -7,21 +7,3 @@ export const concatTimes = <T>(n: number, cb: (i: number) => T): T[] => {
 
   return result
 }
-
-export const getBroadcastIp = (ip: string, subnetMask: string): string => {
-  const ipNumber = ipStrToNumber(ip)
-  const subnetMaskNumber = ipStrToNumber(subnetMask)
-  const broadcastIpNumber = ipNumber | ~subnetMaskNumber
-  return ipNumberToStr(broadcastIpNumber)
-}
-
-const ipStrToNumber = (ip: string): number => {
-  const ipNumbers = ip.split(".").map(Number)
-  return new DataView(Uint8Array.from(ipNumbers).buffer).getUint32(0, true)
-}
-
-const ipNumberToStr = (ipNumber: number): string => {
-  const ipBuffer = Buffer.alloc(4)
-  new DataView(ipBuffer.buffer).setUint32(0, ipNumber, true)
-  return Array.from(ipBuffer).join(".")
-}
